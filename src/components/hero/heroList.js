31c773f80cfb0dfe724d66dd892daaf2ebbfac63
Jorge Cuevas <20152704@itla.edu.do>
2022-05-16 18:52:28 -0400

    adding heroCard Component

--- src/components/hero/heroList.js
+++ src/components/hero/heroList.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import { getHeroesByPublisher } from '../../helpers/getHeroesByPublisher'
+import { HeroCard } from './HeroCard';
 
 export const HeroList = ({ publisher }) => {
   
   const heroListX = getHeroesByPublisher(publisher);   
 
   return (
-    <>
-        <ul>
+    <div className='row rows-cols-1 row-cols-md-3 g-3'>
             {
                 heroListX.map( hero => (
-                    <li key={hero.id}>
-                        {hero.superhero}
-                    </li>
+                    <HeroCard key={hero.id}
+                        {...hero}
+                    />
                 ))
             }
-        </ul>
-    </>
+    </div>
   )
 }
